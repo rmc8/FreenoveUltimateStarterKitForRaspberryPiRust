e1945c952684c98c96c2222f005f100e73e6cf08
rmc-8.com <kmprmc@gmail.com>
2025-06-01 00:24:08 +0900

    Update .gitignore for Rust, macOS, and Linux compatibility
    Revise the .gitignore file to include comprehensive ignore patterns for Rust projects, as well as macOS and Linux system files. Add rules for temporary files, system-specific artifacts, and Cargo.lock to prevent unnecessary tracking. Remove outdated or specific tool-related ignores to streamline the configuration.

---
+++ src/Blink/src/main.rs
@@ -0,0 +1,24 @@
+use rppal::gpio::{Gpio, Level};
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
+
+const LED_PIN: u8 = 17;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Program is starting...");
+    let gpi = Gpio::new()?;
+
+    let mut pin = gpi.get(LED_PIN)?.into_output();
+
+    println!("LED will blink every 1 second.");
+
+    loop {
+        pin.write(Level::High);
+        println!("LED turned on >>>");
+        sleep(Duration::from_secs(1));
+        pin.write(Level::Low);
+        println!("LED turned off <<<");
+        sleep(Duration::from_secs(1));
+    }
+}
