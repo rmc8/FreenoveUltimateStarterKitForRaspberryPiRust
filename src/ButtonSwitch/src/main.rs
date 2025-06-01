2b9dbb3a044c2dff52f787b38b2df216593dc52d
rmc-8.com <kmprmc@gmail.com>
2025-06-01 15:04:11 +0900

    Add ButtonSwitch component for toggle functionality
    Introduce a new ButtonSwitch directory with components to handle toggle or switch behavior in the UI. This addition enhances user interaction by providing a reusable switch element for various settings or options.

---
+++ src/ButtonSwitch/src/main.rs
@@ -0,0 +1,25 @@
+use rppal::gpio::{Gpio, Level};
+use std::error::Error;
+
+const LED_PIN: u8 = 17;
+const BTN_PIN: u8 = 18;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Program is starting...");
+    let gpio = Gpio::new()?;
+    let mut led_pin = gpio.get(LED_PIN)?.into_output();
+    let btn_pin = gpio.get(BTN_PIN)?.into_input();
+    println!("LED will blink every 1 second.");
+
+    loop {
+        if btn_pin.is_low() {
+            // led_pin.set_high();
+            led_pin.write(Level::High);
+            println!("Button is pressed, led turned on >>>");
+        } else {
+            // led_pin.set_low();
+            led_pin.write(Level::Low);
+            println!("Button is released, led turned off <<<");
+        }
+    }
+}
